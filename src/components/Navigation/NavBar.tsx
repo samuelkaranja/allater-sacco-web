cff66173f30826c15bba7449bdd9b256e5dbb1cc
samuelkaranja <samuelkaranja007@gmail.com>
2025-03-01 22:46:55 +0300

    Created and implemented Navigation Bar Component

---
+++ src/components/Navigation/NavBar.tsx
@@ -0,0 +1,16 @@
+import "./navbar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h3>Allater Sacco</h3>
+      </div>
+      <div className="link">
+        <span>Contact Us</span>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
